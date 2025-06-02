c238aaa35e6e73e52d8cd5d00c7f070ed291e300
Nikhil Bachala <147517925+nikhilbachala@users.noreply.github.com>
2025-06-03 00:34:05 +0530

    Update medico.py

--- medico.py
+++ medico.py
@@ -66,8 +66,7 @@ Your expertise is critical in guiding informed decisions. Please proceed with pr
 ]
 
 # Initialize the generative model
-model = genai.GenerativeModel(model_name="gemini-2.5-flash-preview-04-17
-",
+model = genai.GenerativeModel(model_name="gemini-2.0-flash",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
